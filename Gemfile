1a1321c777cea5ce46954f0730c5c2fe678ede59
Victor M Benitez <victormbenitez012@gmail.com>
2017-07-04 20:03:38 -0500

    Goodbey text

--- Gemfile
+++ Gemfile
@@ -2,3 +2,6 @@ source 'https://rubygems.org'
 ruby '2.3.3'
 
 gem 'sinatra', '>= 1.4'
+group :development do
+  gem 'rerun'
+end
---
+++ app.rb
@@ -0,0 +1,7 @@
+require 'sinatra'
+
+class MyApp < Sinatra::Base
+  get '/' do
+    "<!DOCTYPE html><html><head></head><body><h1>Goodbye</h1></body></html>"
+  end
+end
